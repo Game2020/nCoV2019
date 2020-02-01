67ea628df727ff5fcb23f46532396b3a2875ee74
iOSDevLog <iosdevlog@iosdevlog.com>
2020-02-01 20:38:48 +0800

    feat(bar): iframe

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from pyecharts.charts import Bar
 from pyecharts import options as opts
-from flask import Flask
+from flask import Flask, render_template
 from jinja2 import Markup, Environment, FileSystemLoader
 from pyecharts.globals import CurrentConfig
 
@@ -28,18 +28,60 @@ def bar_base() -> Bar:
     )
     return c
 
+
+def bar_china_day_list(china_day_list, last_update_time) -> Bar:
+    c = (
+        Bar()
+        .add_xaxis([item.date for item in china_day_list])
+        .add_yaxis("确诊", [item.confirm for item in china_day_list])
+        .add_yaxis("疑似", [item.suspect for item in china_day_list])
+        .set_global_opts(title_opts=opts.TitleOpts(title="2019 nCoV 确诊/疑似", subtitle=last_update_time))
+    )
+    return c
+
+
+def bar_china_day_list_dead(china_day_list, last_update_time) -> Bar:
+    c = (
+        Bar()
+        .add_xaxis([item.date for item in china_day_list])
+        .add_yaxis("死亡", [item.dead for item in china_day_list])
+        .add_yaxis("治愈", [item.heal for item in china_day_list])
+        .set_global_opts(title_opts=opts.TitleOpts(title="2019 nCoV 死亡/治愈", subtitle=last_update_time))
+    )
+    return c
+
+
 def model_to_str(model):
     attrs = vars(model)
     return '\t'.join("%s:\t%s" % item for item in attrs.items())
 
+
 @app.route("/")
 def index():
+    return render_template('index.html')
+
+
+@app.route("/china_day_list")
+def china_day_list():
     data = fetch_data()
     last_update_time = data.last_update_time
     china_total = data.china_total
+    china_day_list = data.china_day_list
 
-    c = bar_base()
-    return last_update_time + "china_total: " + head + "\n" +  Markup(c.render_embed())
+    # c = bar_base()
+    c = bar_china_day_list(china_day_list, last_update_time)
+    return Markup(c.render_embed())
+
+
+@app.route("/china_day_list_dead")
+def china_day_list_dead():
+    data = fetch_data()
+    last_update_time = data.last_update_time
+    china_total = data.china_total
+    china_day_list = data.china_day_list
+
+    c = bar_china_day_list_dead(china_day_list, last_update_time)
+    return Markup(c.render_embed())
 
 
 if __name__ == "__main__":
